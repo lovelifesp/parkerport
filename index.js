a52211425e6b5bfd203d834f9e0ed42c83f36267
Shana Parker <lovelifesp@gmail.com>
2017-03-06 22:24:49 -0500

    added folders

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 window.alert("Hello!!!");
+
+
+{
+function myFunction() {
+  var x= document .getElementById('topNav');
+}
+if (x.className === "topNav") {
+  x .className =+ "home";
+  }
+else {
+  x .className = "topnav";
+}
+}
